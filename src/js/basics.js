dbbb2d45be94d162e3172e0a7640f72e8141f28e
Sebastian Brümmer <sebastian@produktivbuero.de>
2018-12-06 13:58:59 +0100

    Check if opt out elements exist
    Avoid errors

--- src/js/basics.js
+++ src/js/basics.js
@@ -1,24 +1,26 @@
-var cookieName = window.pb.analytics.cookie.name;
+const cookieName = window.pb.analytics.cookie.name;
+const $analyticsOptOut = document.getElementById('analyticsOptOut');
+const $analyticsStatus = document.getElementById('analyticsStatus');
 
 if (document.cookie.indexOf(cookieName + '=true') > -1) {
   /* global Gooogle Opt Out-Cookie */
   document.cookie = 'ga-disable-'+window.pb.analytics.ga.property + '=true; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/';
   window['ga-disable-'+window.pb.analytics.ga.property] = true;
 
-  document.getElementById('analyticsOptOut').innerHTML = window.pb.analytics.link.enable;
-  document.getElementById('analyticsStatus').innerHTML = window.pb.analytics.status.disabled;
+  if ( $analyticsOptOut !== null ) $analyticsOptOut.innerHTML = window.pb.analytics.link.enable;
+  if ( $analyticsStatus !== null ) $analyticsStatus.innerHTML = window.pb.analytics.status.disabled;
 }
 
 
 function pbAnalyticsOptOut() {
   if (document.cookie.indexOf(cookieName + '=true') > -1) {
     document.cookie = cookieName + '=; ;expires=Thu, 01 Jan 1970 00:00:01 UTC; path=/';
-    document.getElementById('analyticsOptOut').innerHTML = window.pb.analytics.link.disable;
-    document.getElementById('analyticsStatus').innerHTML = window.pb.analytics.status.enabled;
+    $analyticsOptOut.innerHTML = window.pb.analytics.link.disable;
+    $analyticsStatus.innerHTML = window.pb.analytics.status.enabled;
   } else {
     document.cookie = cookieName + '=true; expires=Thu, 31 Dec 2099 23:59:59 UTC; path=/';
     delete window['ga-disable-'+window.pb.analytics.ga.property];
-    document.getElementById('analyticsOptOut').innerHTML = window.pb.analytics.link.enable;
-    document.getElementById('analyticsStatus').innerHTML = window.pb.analytics.status.disabled;
+    $analyticsOptOut.innerHTML = window.pb.analytics.link.enable;
+    $analyticsStatus.innerHTML = window.pb.analytics.status.disabled;
   }
 }
